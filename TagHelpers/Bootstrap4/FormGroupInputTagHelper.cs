be791313922ab426fd6bdcdeb7443e23e6d6c2b5
Shaun Luttin <shaunluttin@gmail.com>
2018-07-21 12:39:06 -0700

    Introduce a select form-group tag helper

--- TagHelpers/Bootstrap4/FormGroupInputTagHelper.cs
+++ TagHelpers/Bootstrap4/FormGroupInputTagHelper.cs
@@ -47,4 +47,24 @@ namespace socialarts.club.TagHelpers.Bootstrap4
             return Task.CompletedTask;
         }
     }
+
+    public class FormGroupSelectTagHelper : FormGroupTagHelperBase {
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output) {
+
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.Add("class", "form-group");
+            output.Content.AppendHtml($"<label for='{Id}'>{Label}</label>");
+
+            // distinct
+            output.Content.AppendHtml($"<select class='form-control' id='{Id}'>");
+
+            var childContent = await output.GetChildContentAsync();
+            var innerHtml = childContent.GetContent();
+            output.Content.AppendHtml(innerHtml);
+
+            output.Content.AppendHtml($"</select>");
+        }
+    }
 }
