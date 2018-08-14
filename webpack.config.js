81aff730b1c512fc200220dc0b4dfa3bceacf79c
Shaun Luttin <shaunluttin@gmail.com>
2018-08-13 17:35:59 -0700

    Load Bootstrap4 with Web Pack - 03

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,29 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './wwwroot'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: 'style-loader', // inject CSS to page
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                }, {
+                    loader: 'postcss-loader', // Run post css actions
+                    options: {
+                        plugins: function () { // post css plugins, can be exported to postcss.config.js
+                            return [
+                                require('precss'),
+                                require('autoprefixer')
+                            ];
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader' // compiles Sass to CSS
+                }]
+            }
+        ]
     }
 }
