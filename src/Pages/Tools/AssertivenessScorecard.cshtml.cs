3549025585eb942f853c3eb83c42be912057e3d0
Shaun Luttin <shaunluttin@gmail.com>
2018-08-14 16:40:44 -0700

    Implement Post/Redirect/Get

--- src/Pages/Tools/AssertivenessScorecard.cshtml.cs
+++ src/Pages/Tools/AssertivenessScorecard.cshtml.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace socialarts.club.Pages.Tools
 {
     public class AssertivenessScorecardModel : PageModel
     {
+        public async Task<IActionResult> OnPostAsync() 
+        {
+            await Task.CompletedTask;
+
+            // Post/Redirect/Get to avoid multiple form submission
+            return RedirectToPage(HttpContext.Request.Path);
+        }
     }
 }
