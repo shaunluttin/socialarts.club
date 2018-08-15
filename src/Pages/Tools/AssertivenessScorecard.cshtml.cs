65cd82a3394f597ef91bf5fe0fb9ef75084d66ee
Shaun Luttin <shaunluttin@gmail.com>
2018-08-14 19:23:47 -0700

    Bind AssertivenessScorecard

--- src/Pages/Tools/AssertivenessScorecard.cshtml.cs
+++ src/Pages/Tools/AssertivenessScorecard.cshtml.cs
@@ -11,11 +11,28 @@ namespace socialarts.club.Pages.Tools
     [BindProperties]
     public class AssertivenessScorecardModel : PageModel
     {
-        public DateTime Date { get ; set; }
+        public string Date { get ; set; }
+
+        public string Time { get ; set; }
+
+        public string Place {get; set; }
+
+        public string PersonOrSituation {get; set; }
+
+        public string YourResponse {get; set; }
+
+        public string YourAssessment {get; set; }
+
+        public string Outcome {get; set; }
+
+        public string FeelingsAfter {get; set; }
+
+        public string AlternativeResponse {get; set; }
 
         public async Task<IActionResult> OnPostAsync() 
         {
             await Task.CompletedTask;
+            HttpContext.Request.Form.Dump();
 
             // Post/Redirect/Get to avoid multiple form submission
             return RedirectToPage(HttpContext.Request.Path);
