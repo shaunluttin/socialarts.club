f518346d545fd9565c53018941a9e71a5e0c588d
Shaun Luttin <shaunluttin@gmail.com>
2018-08-14 19:36:26 -0700

    Bind the select element

--- src/Pages/Tools/AssertivenessScorecard.cshtml.cs
+++ src/Pages/Tools/AssertivenessScorecard.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using socialarts.club.ViewComponents.Extensions;
 
 namespace socialarts.club.Pages.Tools
@@ -11,25 +12,34 @@ namespace socialarts.club.Pages.Tools
     [BindProperties]
     public class AssertivenessScorecardModel : PageModel
     {
-        public string Date { get ; set; }
+        public string Date { get; set; }
 
-        public string Time { get ; set; }
+        public string Time { get; set; }
 
-        public string Place {get; set; }
+        public string Place { get; set; }
 
-        public string PersonOrSituation {get; set; }
+        public string PersonOrSituation { get; set; }
 
-        public string YourResponse {get; set; }
+        public string YourResponse { get; set; }
 
-        public string YourAssessment {get; set; }
+        public string YourAssessment { get; set; }
 
-        public string Outcome {get; set; }
+        public List<SelectListItem> AssessmentOptions { get; set; }
+            = new List<SelectListItem>
+            {
+                new SelectListItem("Assertive", "assertive"),
+                new SelectListItem("Passive", "passive"),
+                new SelectListItem("Aggressive", "aggressive"),
+                new SelectListItem("Passive/Aggressive", "p/a"),
+            };
 
-        public string FeelingsAfter {get; set; }
+        public string Outcome { get; set; }
 
-        public string AlternativeResponse {get; set; }
+        public string FeelingsAfter { get; set; }
 
-        public async Task<IActionResult> OnPostAsync() 
+        public string AlternativeResponse { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
         {
             await Task.CompletedTask;
             HttpContext.Request.Form.Dump();
