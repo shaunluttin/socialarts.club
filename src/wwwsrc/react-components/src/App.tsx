ecd5ddeee06fd774763abf326d687fb61cc93338
Shaun Luttin <shaunluttin@gmail.com>
2018-08-28 11:59:23 -0700

    Render tools from state

--- src/wwwsrc/react-components/src/App.tsx
+++ src/wwwsrc/react-components/src/App.tsx
@@ -3,12 +3,23 @@ import ToolsList, { Tool } from "./components/ToolsList";
 
 class App extends React.Component {
 
+  public state = {
+    tools: [] as Tool[]
+  }
+
   public render() {
     return (
       <div className="App">
-        <ToolsList Tools={this.getTools()} />
+        <ToolsList Tools={this.state.tools} />
       </div>
     );
+  }
+
+  public componentDidMount() {
+    const tools = this.getTools();
+    this.setState({
+      tools
+    });
   }
 
   private getTools(): Tool[] {
