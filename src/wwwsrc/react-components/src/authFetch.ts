8df1866585f20861fbad8f8baf66ce4734911f4b
Shaun Luttin <shaunluttin@gmail.com>
2018-09-02 15:56:34 -0700

    Configure the UserManager

--- src/wwwsrc/react-components/src/authFetch.ts
+++ src/wwwsrc/react-components/src/authFetch.ts
@@ -1,19 +1,47 @@
 import { User, UserManager, UserManagerSettings } from 'oidc-client';
 
-const settings: UserManagerSettings = {}; 
-const userManager = new UserManager(settings);
-
 console.log('foo');
 
-let user: User;
-userManager.getUser()
-    .then((response) => user = response)
-    .catch((err) => console.log(err));
+const host = window.location.origin;
 
-export default (input?: Request | string, init?: RequestInit) : Promise<Response> => {
+//
+// Configure the oidc-client.js UserManager.
+//
+const settings: UserManagerSettings = {
+    authority: host,
+    client_id: 'socialarts.club',
+    loadUserInfo: false,
+    response_type: 'token',
+    scope: 'openid',
+    silentRequestTimeout: 10000,
+    silent_redirect_uri: `${host}/Me`
+};
+
+const userManager = new UserManager(settings);
+
+if (window !== window.parent) {
+    userManager.signinSilentCallback();
+}
+
+const getAccessToken = async (): Promise<string> => {
+    try {
+        let user: User = await userManager.getUser();
+        if (user) {
+            return user.access_token;
+        }
+
+        user = await userManager.signinSilent();
+        return user.access_token;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export default async (input?: Request | string, init?: RequestInit): Promise<Response> => {
     console.log('baz');
-    // get the access token from local storage if it exists
-    // otherwise, perform a silent sign in to get the access token
+
+    const token = await getAccessToken();
+
     return fetch(input, init);
 };
 
