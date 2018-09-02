5e1278ac93e76cb7c403eb411a842a0802e42d97
Shaun Luttin <shaunluttin@gmail.com>
2018-09-02 15:25:51 -0700

    Install oidc-client

--- src/wwwsrc/react-components/src/fetchAuth.ts
+++ src/wwwsrc/react-components/src/fetchAuth.ts
@@ -1,3 +1,7 @@
+import oidc from 'oidc-client';
+
 export default (input?: Request | string, init?: RequestInit) : Promise<Response> => {
+    // get the access token from local storage if it exists
+    // otherwise, perform a silent sign in to get the access token
     return fetch(input, init);
 };
