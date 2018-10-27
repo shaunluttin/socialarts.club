4e565be8b2a013b6cbfe23d779f3a1907546fdb8
Shaun Luttin <shaunluttin@gmail.com>
2018-10-27 14:50:28 -0700

    Introduce ToolsPageModel

--- src/Pages/Tools/AssertivenessScorecard.cshtml.cs
+++ src/Pages/Tools/AssertivenessScorecard.cshtml.cs
@@ -12,14 +12,13 @@ using socialarts.club.ViewComponents.Extensions;
 
 namespace socialarts.club.Pages.Tools
 {
-    public class AssertivenessScorecardModel : PageModel
+    public class AssertivenessScorecardModel : ToolsPageModel<AssertivenessScorecard>
     {
-        private readonly ApplicationDbContext db;
-
-        private readonly UserManager<IdentityUser> userManager;
-
-        [BindProperty]
-        public AssertivenessScorecard Form { get; set; }
+        public AssertivenessScorecardModel(
+            ApplicationDbContext db,
+            UserManager<IdentityUser> userManager) : base(db, userManager)
+        {
+        }
 
         public List<SelectListItem> AssessmentOptions { get; set; }
             = new List<SelectListItem>
@@ -29,66 +28,5 @@ namespace socialarts.club.Pages.Tools
                 new SelectListItem("Aggressive", "aggressive"),
                 new SelectListItem("Passive/Aggressive", "p/a"),
             };
-
-        public bool Disabled { get; set; }
-
-        public AssertivenessScorecardModel(
-            ApplicationDbContext db,
-            UserManager<IdentityUser> userManager)
-        {
-            this.db = db;
-            this.userManager = userManager;
-        }
-
-        public async Task OnGetAsync(int id = 0)
-        {
-            if (id <= 0) return;
-
-            var currentUserId = userManager.GetUserId(User);
-
-            var doc = await db.ToolsDocument.FindAsync(id);
-
-            if (doc == null)
-            {
-                // return 404 not found (or other appropriate status).
-            }
-            else if (doc.OwnerId != currentUserId)
-            {
-                // return 403 forbidden (or other appropriate status).
-            }
-
-            Disabled = true;
-            Form = JsonConvert.DeserializeObject<AssertivenessScorecard>(doc.Json);
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            var doc = await SaveToolsDocument();
-
-            // Post/Redirect/Get to avoid multiple form submission
-            return RedirectToPage(HttpContext.Request.Path, new { doc.Id });
-        }
-
-        public async Task<ToolsDocument> SaveToolsDocument()
-        {
-            var currentUserId = userManager.GetUserId(User);
-
-            // ~/Tools/AssertivenessScorecard
-            var razorPage = PageContext.ActionDescriptor.DisplayName;
-            var name = razorPage.Split('/').Last();
-
-            var doc = new ToolsDocument
-            {
-                Name = name,
-                TemplateUrlPath = razorPage,
-                OwnerId = currentUserId,
-                Json = JsonConvert.SerializeObject(Form),
-            };
-
-            var result = db.ToolsDocument.Add(doc);
-            await db.SaveChangesAsync();
-
-            return doc;
-        }
     }
 }
--- src/Pages/Tools/ToolsPageModel.cs
+++ src/Pages/Tools/ToolsPageModel.cs
@@ -1,7 +1,86 @@
-
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using socialarts.club.Data;
 
-public class ToolsPageModel : PageModel
+public abstract class ToolsPageModel<T> : PageModel
 {
+    [BindProperty]
+    public T Form { get; set; }
 
+    private ApplicationDbContext db { get; }
+
+    protected readonly UserManager<IdentityUser> userManager;
+
+    public ToolsPageModel(
+        ApplicationDbContext db,
+        UserManager<IdentityUser> userManager)
+    {
+        this.userManager = userManager;
+        this.db = db;
+    }
+
+    public bool Disabled { get; set; }
+
+    public async Task OnGetAsync(int id = 0)
+    {
+        var doc = await GetToolsDocument(id);
+
+        var currentUserId = userManager.GetUserId(User);
+
+        if (doc == null)
+        {
+            // return 404 not found (or other appropriate status).
+        }
+        else if (doc.OwnerId != currentUserId)
+        {
+            // return 403 forbidden (or other appropriate status).
+        }
+
+        Disabled = true;
+        Form = JsonConvert.DeserializeObject<T>(doc.Json);
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        var doc = await SaveToolsDocument();
+
+        // Post/Redirect/Get to avoid multiple form submission
+        return RedirectToPage(HttpContext.Request.Path, new { doc.Id });
+    }
+
+    private async Task<ToolsDocument> SaveToolsDocument()
+    {
+        var currentUserId = userManager.GetUserId(User);
+
+        // ~/Tools/AssertivenessScorecard
+        var razorPage = PageContext.ActionDescriptor.DisplayName;
+        var name = razorPage.Split('/').Last();
+
+        var doc = new ToolsDocument
+        {
+            Name = name,
+            TemplateUrlPath = razorPage,
+            OwnerId = currentUserId,
+            Json = JsonConvert.SerializeObject(Form),
+        };
+
+        var result = db.ToolsDocument.Add(doc);
+        await db.SaveChangesAsync();
+
+        return doc;
+    }
+
+    private async Task<ToolsDocument> GetToolsDocument(int id)
+    {
+        if (id <= 0) return null;
+
+        var currentUserId = userManager.GetUserId(User);
+        var doc = await db.ToolsDocument.FindAsync(id);
+
+        return doc;
+    }
 }
