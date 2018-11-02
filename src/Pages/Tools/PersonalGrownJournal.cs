cf3ed4a9405c72f316932722cff1dfabf7331a40
Shaun Luttin <shaunluttin@gmail.com>
2018-11-02 14:02:49 -0700

    Update PersonalGrowthJournal

---
+++ src/Pages/Tools/PersonalGrownJournal.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace socialarts.club.Pages.Tools
+{
+    public class PersionalGrowthJournal
+    {
+        public DateTime Date { get; set; }
+
+        public string Situations { get; set; }
+        
+        public string People { get; set; }
+        
+        public string Attitudes { get; set; }
+        
+        public string Behaviors { get; set; }
+        
+        public string Obstacles { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
---
+++ src/Pages/Tools/PersonalGrowthJournal.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using socialarts.club.Data;
+
+namespace socialarts.club.Pages.Tools
+{
+    public class PersonalGrowthJournalModel : ToolsPageModel<PersionalGrowthJournal>
+    {
+        public override string CitationAuthor => "Alberti";
+
+        public override string CitationYear => "1995";
+
+        public PersonalGrowthJournalModel(
+            ApplicationDbContext db,
+            UserManager<IdentityUser> userManager) : base(db, userManager)
+        {
+        }
+    }
+}
