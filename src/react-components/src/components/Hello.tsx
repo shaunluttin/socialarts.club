9778d3d2b8ed1de28e5a2010bbf3496cdff5c605
Shaun Luttin <shaunluttin@gmail.com>
2019-05-10 15:42:55 -0700

    Add the same React component twice

--- src/react-components/src/components/Hello.tsx
+++ src/react-components/src/components/Hello.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-export interface HelloProps { compiler: string; framework: string; }
+interface HelloProps { name: string; }
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+const Hello = (props: HelloProps) => <h1>Hello from {props.name}.</h1>;
 
-document.querySelectorAll('.react-example')
+document
+    .querySelectorAll('.react-example')
+    .forEach((domContainer: any) => {
+        const name = domContainer.dataset.name;
+        const element = React.createElement(Hello, { name: name });
+        ReactDOM.render(element, domContainer);
+    })
--- src/react-components/src/index.tsx
+++ src/react-components/src/index.tsx
@@ -1,9 +1,1 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import { Hello } from "./components/Hello";
-
-ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
-);
+import "./components/Hello";
